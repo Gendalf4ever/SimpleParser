669ee4919b13fcc9d094e0ba00c0728661b5ae7a
Nikita Yurishev <chelovek438@gmail.com>
2024-02-09 09:03:49 +0300

    some tests

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-$url = 'https://e-disclosure.ru/poisk-po-soobshheniyam';
-$file = file_get_contents($url);
-echo $file;
-$pattern = '#form id = sEventSearchForm.+?</form>#s';
-preg_match($pattern, $file, $matches);
+$url = "https://e-disclosure.ru/poisk-po-soobshheniyam";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false); 
+$response = curl_exec($ch);
+
+if(curl_errno($ch)){
+    echo 'Curl error: ' . curl_error($ch);
+}
+
+curl_close($ch);
+
+echo $response;
 
 ?>
